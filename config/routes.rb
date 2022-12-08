3b501e5c3b28914e10bca4ca4af87d15895a3dc4
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-12-08 21:16:02 +0300

    Update: technologies route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
    namespace :api do
     namespace :v1 do
-     resources :projects do
-      get "technologies", to: "technologies"
-     end
      resources :technologies, only: %i[index]
      resources :messages, only: %i[index show create]
      get "get_authentication_token", to: "authentications#form_token"
